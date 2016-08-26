3d0cb4c8a2fbdde6a4077340018c495c2081ef10
Meghaditya Roy Chaudhury <meroycha@microsoft.com>
2016-08-26 17:35:57 +0530

    modifying stream example file

--- stream.py
+++ stream.py
@@ -1,19 +1,20 @@
 from twython import TwythonStreamer
-
+from io_helper import twitter_credential_prompt
+from io_helper import custom_prompt
 
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
-            print data['text'].encode('utf-8')
+            print(data['text'].encode('utf-8'))
         # Want to disconnect after the first result?
         # self.disconnect()
 
     def on_error(self, status_code, data):
-        print status_code, data
+        print(status_code, data)
 
 # Requires Authentication as of Twitter API v1.1
-stream = MyStreamer(APP_KEY, APP_SECRET,
-                    OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET) = twitter_credential_prompt()
+stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 stream.statuses.filter(track='twitter')
 # stream.user()
